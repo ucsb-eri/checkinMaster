44150a1736e2b9a8a4f92887b9e77d1745bef2bb
Aaron Martin <sbbeachvball@gmail.com>
2016-01-07 12:02:16 -0800

    Some quick initial setup/prep

--- routes/checkin.js
+++ routes/checkin.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+    // response here doesn't really matter, just want to log the goodies into
+    // the db
   res.send('respond with a resource');
 });
 
