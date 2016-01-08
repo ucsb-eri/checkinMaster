f25a41d218ada00ec1e1008fef3fec73ab9d4a02
Aaron Martin <sbbeachvball@gmail.com>
2016-01-07 22:58:08 -0800

    more changes to install.sh script

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+groupadd -g 500 nsmgmt
+useradd -u 500 -c "Nsmgmt account to run stuff" nsmgmt
+
 dbpath=/opt/dbs/checkin.sqlite3
 dname=`dirname $dbpath`
 mkdir -p $dname
 chmod 777 $dname
+touch $dbpath
+chmod 666 $dbpath
+chown -R nsmgmt:nsmgmt $dname
 
 cat <<EOF > './config.json'
 {
@@ -15,9 +21,6 @@ npm install
 
 allowfrom="128.111.100.0/23"
 echo "-A INPUT -m state --state NEW -m tcp -p tcp --dport 3000:3005 -s $allowfrom -j ACCEPT"
-
-groupadd -g 500 nsmgmt
-useradd -u 500 -c "Nsmgmt account to run stuff" nsmgmt
 
 svc=/usr/lib/systemd/system
 if [ -d $svc ]; then
