35e03874d3921a23b32ea125c94083acab04a963
Aaron Martin <sbbeachvball@gmail.com>
2016-01-07 23:11:41 -0800

    update output log format

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
