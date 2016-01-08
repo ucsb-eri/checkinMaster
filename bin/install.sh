1832449c020d0ee9a4157feafa5efc6419615712
Aaron Martin <sbbeachvball@gmail.com>
2016-01-07 20:27:30 -0800

    more tweaks to install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -16,7 +16,14 @@ npm install
 allowfrom="128.111.100.0/23"
 echo "-A INPUT -m state --state NEW -m tcp -p tcp --dport 3000:3005 -s $allowfrom -j ACCEPT"
 
-svc=/etc/systemd/system
+groupadd -g 500 nsmgmt
+useradd -u 500 -c "Nsmgmt account to run stuff" nsmgmt
+
+svc=/usr/lib/systemd/system
 if [ -d $svc ]; then
     echo "cp svc/node-checkin-master.service $svc"
+    cp svc/node-checkin-master.service $svc
+    
+    systemctl enable node-checkin-master
+    systemctl start node-checkin-master
 fi
